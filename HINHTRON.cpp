b885114464639c07b372945b5243d0ce3926b13b
duyanh020405 <147359622+duyanh020405@users.noreply.github.com>
2023-11-07 23:12:46 +0700

    Add files via upload

---
+++ HINHTRON.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main(){
+	float r;
+	r=2; 
+	float PI ;
+	PI = 3.14;
+	 float dientichhinhtron = r*r * PI ; 
+	 float  chuvihinhtron  = 2*r * PI; 
+} 
---
+++ hinh vuong.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int main(){
+	float chieudaihcn , chieuronghcn ;
+	chieudaihcn = 10 , chieuronghcn =5 ;
+	float dientichhcn = chieudaihcn * chieuronghcn ;
+	float chuvihcn = ( chieudaihcn + chieuronghcn ) / 2 ; 
+} 
